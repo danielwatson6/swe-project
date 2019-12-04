ce43d5c0993c5f0e78c2f5b0669048326c601644
Daniel Watson <daniel.watson@nyu.edu>
2019-12-04 15:08:52 +0530

    Using high-level DB methods

--- src/scripts/firebase_upload.py
+++ src/scripts/firebase_upload.py
@@ -1,20 +1,14 @@
+import argparse
 import csv
 import os
 import re
-import logging
 
-from db import db
+from db import BoardMembers, Matches, Mentees, Mentors
 
 
 def snake_case(s):
     s = s.lower().strip()
     return re.sub(r"\s+", r"_", s)
-
-
-def wipe_collection(collection):
-    """Deletes all documents inside a Firebase collection."""
-    for doc in collection.stream():
-        collection.document(doc.id).delete()
 
 
 def read_csv(path):
@@ -29,100 +23,100 @@ def read_csv(path):
                 yield {k: row[i] for i, k in enumerate(fields) if k}
 
 
-def handle_mentors(overwrite_=True, path_="Mentors.csv"):
-    mentors_ref = db.collection("mentors")
-    if overwrite_:
-        wipe_collection(mentors_ref)
+def handle_mentors(overwrite, path):
+    if overwrite:
+        Mentors.wipe()
 
-    batch = db.batch()
-
-    for row in read_csv(os.path.join("data", path_)):
+    mentors = {}
+    for row in read_csv(os.path.join("data", path)):
         try:
             row["mentees"] = []
-            id = row["email"]  # use emails as unique id
-            batch.set(mentors_ref.document(id), row)
+            email = row["email"]
+            del row["email"]
+            mentors[email] = row
         except KeyError as e:
             print("Warning: row with missing email key: ", e)
 
-    batch.commit()
+    Mentors.add_batch(mentors)
 
 
-def handle_mentees(overwrite_=True, path_="Mentees.csv"):
-    mentees_ref = db.collection("mentees")
-    if overwrite_:
-        wipe_collection(mentees_ref)
+def handle_mentees(overwrite, path):
+    if overwrite:
+        Mentees.wipe()
 
-    batch = db.batch()
-
-    for row in read_csv(os.path.join("data", path_)):
+    mentees = {}
+    for row in read_csv(os.path.join("data", path)):
         try:
             row["mentor"] = ""
-            id = row["email"]  # use emails as unique id
-            batch.set(mentees_ref.document(id), row)
+            email = row["email"]
+            del row["email"]
+            mentees[email] = row
         except KeyError as e:
             print("Warning: row with missing email key: ", e)
 
-    batch.commit()
+    Mentees.add_batch(mentees)
 
 
-def handle_board_members(overwrite_=True, path_="Board_Members.csv"):
-    bm_ref = db.collection("board_members")
-    if overwrite_:
-        wipe_collection(bm_ref)
+def handle_board_members(overwrite, path):
+    if overwrite:
+        BoardMembers.wipe()
 
-    batch = db.batch()
-
-    for row in read_csv(os.path.join("data", path_)):
+    board_members = {}
+    for row in read_csv(os.path.join("data", path)):
         try:
-            id = row["email"]  # use emails as unique id
-            batch.set(bm_ref.document(id), row)
+            email = row["email"]
+            del row["email"]
+            board_members[email] = row
         except KeyError as e:
             print("Warning: row with missing email key: ", e)
 
-    batch.commit()
+    BoardMembers.add_batch()
 
 
-def handle_matches(overwrite_=True, path_="Matches.csv"):
-    mentors_ref = db.collection("mentors")
-    mentees_ref = db.collection("mentees")
-    matches_ref = db.collection("matches")
-
-    if overwrite_:
-        wipe_collection(matches_ref)
-
-    batch = db.batch()
+def handle_matches(path_="Matches.csv"):
 
     for row in read_csv(os.path.join("data", path_)):
         try:
             mentor_email = row["mentor_email"]
             mentee_email = row["mentee_email"]
-            try:
-                mentor_ref = mentors_ref.document(mentor_email)
-                mentee_ref = mentees_ref.document(mentee_email)
-                mentor = mentor_ref.get()
-                mentee = mentee_ref.get()
-                if mentor.exists and mentee.exists:
-                    mentor_mentees = mentor.to_dict()["mentees"]
-                    mentor_mentees.append(mentee.id)
-                    id = mentor.id + mentee.id  # use
-                    batch.set(matches_ref.document(id), row)
-                    batch.update(mentor_ref, {"mentees": mentor_mentees})
-                    batch.update(mentee_ref, {"mentor": mentor.id})
-                else:
-                    raise StopIteration
-            except StopIteration:
-                print("Warning: someone with one of these two emails doesn't exist:")
-                print(mentor_email)
-                print(mentee_email)
         except KeyError as e:
             print("Warning: row with missing mentor or mentee email key: ", e)
+        try:
+            Matches.add(mentor_email, mentee_email)
+        except ValueError as e:
+            print("Warning: mentor or mentee not found.", e)
 
-    batch.commit()
+
+def str2bool(s):
+    """Sanitize strings for argparse.
+
+    Taken from:
+    https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+
+    """
+    if isinstance(s, bool):
+        return s
+    if s.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    if s.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    raise argparse.ArgumentTypeError("Boolean value expected.")
 
 
 if __name__ == "__main__":
-    handle_mentors()
-    handle_mentors(False, "New_Mentors.csv")
-    handle_mentees()
-    handle_board_members()
-    handle_matches()
+    parser = argparse.ArgumentParser()
+    parser.add("--wipe_mentors", type=str2bool, nargs="?", default=True)
+    parser.add("--wipe_mentees", type=str2bool, nargs="?", default=True)
+    parser.add("--wipe_board_members", type=str2bool, nargs="?", default=True)
+    parser.add("--mentors_csv", type=str, default="Mentors.csv")
+    parser.add("--mentees_csv", type=str, default="Mentees.csv")
+    parser.add("--matches_csv", type=str, default="Matches.csv")
+    parser.add(
+        "--board_members_csv", type=str, default="BoardMembers.csv",
+    )
+    FLAGS = parser.parse_args()
+
+    handle_mentors(FLAGS.wipe_mentors, FLAGS.mentors_csv)
+    handle_mentees(FLAGS.wipe_mentees, FLAGS.mentees_csv)
+    handle_matches(FLAGS.matches_csv)
+    handle_board_members(FLAGS.wipe_board_members, FLAGS.board_members_csv)
