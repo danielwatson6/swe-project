a8283a5ff8f8d718b7489ff63e6f0edc87eabbc8
Daniel Watson <daniel.watson@nyu.edu>
2019-12-06 16:03:31 +0400

    Added to_dict abstraction to enable hiding stuff from client

--- src/db/mentees.py
+++ src/db/mentees.py
@@ -3,3 +3,11 @@ from db import Collection
 
 class Mentees(Collection):
     collection = "mentees"
+
+    @classmethod
+    def to_dict(cls, doc):
+        d = super().to_dict(doc)
+        d["name"] = f'{d["first_name"]} {d["last_name"]}'
+        del d["first_name"]
+        del d["last_name"]
+        return d
