fe75c559e723b1d2c51ab349338e9b11e363eedb
Daniel Watson <daniel.watson@nyu.edu>
2019-12-06 17:59:01 +0400

    Finished mentees view

---
+++ src/client/App/Home/Mentees/Mentee/Mentee.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ({ email, mentee, selected, handleSelect }) {
+    return (
+        <tr id={email}>
+            <td>
+                <input type="checkbox" onChange={handleSelect} checked={selected} />
+            </td>
+            <td>{mentee.name}</td>
+            <td>{mentee.net_id}</td>
+            <td>{email}</td>
+            <td><a href={`#${mentee.mentor}`}>{mentee.mentor}</a></td>
+            <td>{mentee.major}</td>
+            <td>{mentee.graduation_date}</td>
+            <td>{mentee.nationality}</td>
+        </tr>
+    );
+};
