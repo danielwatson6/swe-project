2d7b5df76d7c26d56838c6e92bcea485d9156233
Daniel Watson <daniel.watson@nyu.edu>
2019-12-08 18:20:06 +0400

    Updated delete module name

--- src/server/routes/delete_people.py
+++ src/server/routes/delete_people.py
@@ -8,7 +8,7 @@ from server.utils import check_json_request, check_session
 @check_session
 @check_json_request({"mentees": list, "mentors": list})
 @app.route("/delete", methods=["DELETE"])
-def mentees():
+def delete_people():
     emails = request.get_json()
     Mentees.delete_batch(emails["mentees"])
     Mentors.delete_batch(emails["mentors"])
---
+++ src/server/routes/download.py
@@ -0,0 +1,15 @@
+from flask import jsonify, request
+
+from db import Mentees, Mentors
+from server import app
+from server.utils import check_json_request, check_session
+
+
+@check_session
+@check_json_request({"mentees": list, "mentors": list})
+@app.route("/delete", methods=["DELETE"])
+def download():
+    emails = request.get_json()
+    Mentees.delete_batch(emails["mentees"])
+    Mentors.delete_batch(emails["mentors"])
+    return jsonify()
